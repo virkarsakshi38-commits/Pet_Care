aad312f663f13a04795b5ed36a9af38278e0aa93
virkarsakshi38-commits <virkarsakshi38@gmail.com>
2026-01-20 12:54:23 +0530

    Update MySQL connection details

--- connection.js
+++ connection.js
@@ -3,10 +3,10 @@ var util = require("util");
 
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "pet_care_website",
+    host: "bmaa2u5mxjfx53u6cjax-mysql.services.clever-cloud.com",
+    user: "ujvinsxeubrjvp2w",
+    password: "tBQEUWQOJZ8zsyNdF0oR",
+    database: "bmaa2u5mxjfx53u6cjax",
 })
 const exe = util.promisify(conn.query).bind(conn);
 
